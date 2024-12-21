5b339847ba9e1176f834964acbb35e8bc32d523e
RyanPotat <ryan.serenity@gmail.com>
2024-12-21 10:13:57 +0000

    impr: use redis for rate limiting per ip

--- api/middleware/ratelimit.go
+++ api/middleware/ratelimit.go
@@ -1,47 +1,92 @@
 package middleware
 
 import (
-	"net/http"
 	"time"
+	"strconv"
+	"context"
+	"net/http"
 
-	"golang.org/x/time/rate"
+	"potat-api/common/db"
 )
 
-var globalLimiter = rate.NewLimiter(
-	rate.Every(100 * time.Second), 
-	200,
-)
+
+// TODO make this configurable per route or something
 
 func GlobalLimiter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !globalLimiter.Allow() {
+		ip := r.RemoteAddr
+		if forwardedFor := r.Header.Get("CF-Connecting-IP"); forwardedFor != "" {
+			ip = forwardedFor
+		}
+
+		allowed, remaining, remainingTTL, err := getIpToken(
+			ip,
+			r.Context(),
+			100,
+			1 * time.Minute,
+		)
+
+		if err != nil {
 			http.Error(
-				w, 
-				http.StatusText(http.StatusTooManyRequests), 
+				w,
+				http.StatusText(http.StatusInternalServerError),
+				http.StatusInternalServerError,
+			)
+		}
+
+		w.Header().Set("X-RateLimit-Reset", strconv.FormatInt(remainingTTL, 10))
+		w.Header().Set("X-RateLimit-Limit", "100")
+		w.Header().Set("X-RateLimit-Remaining", strconv.FormatInt(remaining, 10))
+
+		if !allowed {
+			http.Error(
+				w,
+				http.StatusText(http.StatusTooManyRequests),
 				http.StatusTooManyRequests,
 			)
-
-			return
 		}
+		
+		w.Header().Set("X-Forwarded-For", ip)
 
 		next.ServeHTTP(w, r)
 	})
 }
 
-func SpecificLimiter(limiter *rate.Limiter) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			if !limiter.Allow() {
-				http.Error(
-					w,
-					http.StatusText(http.StatusTooManyRequests),
-					http.StatusTooManyRequests,
-				)
+func getIpToken(
+	ip string,
+	ctx context.Context,
+	limit int64,
+	window time.Duration,
+) (bool, int64, int64, error) {
+	// Set key expiry if its first request from an ip
+	luaScript := `
+		local current = redis.call("INCR", KEYS[1])
+		if current == 1 then
+				redis.call("EXPIRE", KEYS[1], ARGV[1])
+		end
 
-				return
-			}
+		local ttl = redis.call("TTL", KEYS[1])
+		local allowed = current <= tonumber(ARGV[2])
 
-			next.ServeHTTP(w, r)
-		})
+		return {current, allowed, ttl}
+	`
+
+	result, err := db.Redis.Eval(
+		ctx,
+		luaScript,
+		[]string{ip},
+		int(window.Seconds()),
+		limit,
+	).Result()
+
+	if err != nil {
+		return false, 0, 0, err
 	}
+
+	results := result.([]interface{})
+	remaining := limit - results[0].(int64)
+	allowed := results[1].(int64) == 1
+	remainingTTL := results[2].(int64)  
+
+	return allowed, remaining, remainingTTL, nil
 }
